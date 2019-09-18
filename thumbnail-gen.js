4eb3a968cd8afa17bd2db86a35cb08003e6c56e7
Gregg Tavares <github@greggman.com>
2019-09-19 00:25:43 +0900

    use font

--- thumbnail-gen.js
+++ thumbnail-gen.js
@@ -19,42 +19,53 @@
       return `hsl(${h * 360 | 0},${s * 100}%,${l * 100}%)`;
     }
 
-    function drawParagraph(ctx, text, x, y, stroke) {
-      const words = text.split(' ');
-      let str = words.shift();
-      do {
-        const word = words.shift() || '';
-        const newStr = `${str} ${word}`;
-        const m = ctx.measureText(newStr);
-        if (m.width > 1000 || !word) {
-          if (stroke) {
-            ctx.strokeText(str, x, y);
-          } else {
-            ctx.fillText(str, x, y);
-          }
-          y += 100;
-          str = word;
-        } else {
-          str = newStr;
-        }
-      } while (str);
-    }
-
-    function drawShadowedParagraph(ctx, text, x, y) {
-      const color = ctx.fillStyle;
-      ctx.strokeStyle = 'black';
-      ctx.fillStyle = 'black';
-      drawParagraph(ctx, text, x + 15, y + 15, true);
-      drawParagraph(ctx, text, x + 15, y + 15);
-      ctx.strokeStyle = 'white';
-      ctx.lineWidth = 15;
-      drawParagraph(ctx, text, x, y, true);
-      ctx.fillStyle = color;
-      drawParagraph(ctx, text, x, y);
-    }
-
     return function genThumbnail(options) {
-      const {backgroundImage, canvas, text} = options;
+      const {
+        backgroundImage,
+        canvas,
+        text,
+        font,
+        verticalSpacing, // 100
+        offset,  // 100, 120
+        shadowOffset,  // 15
+        strokeWidth,  // 15
+        textWrapWidth,  // 1000
+      } = options;
+
+      function drawParagraph(ctx, text, x, y, stroke) {
+        const words = text.split(' ');
+        let str = words.shift();
+        do {
+          const word = words.shift() || '';
+          const newStr = `${str} ${word}`;
+          const m = ctx.measureText(newStr);
+          if (m.width > textWrapWidth || !word) {
+            if (stroke) {
+              ctx.strokeText(str, x, y);
+            } else {
+              ctx.fillText(str, x, y);
+            }
+            y += verticalSpacing;
+            str = word;
+          } else {
+            str = newStr;
+          }
+        } while (str);
+      }
+
+      function drawShadowedParagraph(ctx, text, x, y) {
+        const color = ctx.fillStyle;
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'black';
+        drawParagraph(ctx, text, x + shadowOffset[0], y + shadowOffset[1], true);
+        drawParagraph(ctx, text, x + shadowOffset[0], y + shadowOffset[1]);
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = strokeWidth;
+        drawParagraph(ctx, text, x, y, true);
+        ctx.fillStyle = color;
+        drawParagraph(ctx, text, x, y);
+      }
+      
       const ctx = canvas.getContext('2d');
       ctx.drawImage(backgroundImage, 0, 0);
 
@@ -63,8 +74,8 @@
         s += text.codePointAt(i);
       }
 
-      ctx.font = 'bold 100px sans-serif';
+      ctx.font = font;
       ctx.fillStyle = hsl(s / 100 % 1, 1, 0.4);
-      drawShadowedParagraph(ctx, text, 100, 120);
+      drawShadowedParagraph(ctx, text, offset[0], offset[1]);
     };
 }));
