a48e301dfca3ceca21ff8b8149f9d9e66bc7f3bb
Gregg Tavares <github@greggman.com>
2019-10-07 00:31:06 +0900

    add more text

--- thumbnail-gen.js
+++ thumbnail-gen.js
@@ -24,72 +24,92 @@
         backgroundImage,
         canvas,
         text,
-        font,
-        verticalSpacing, // 100
-        offset,  // 100, 120
-        shadowOffset,  // 15
-        strokeWidth,  // 15
-        textWrapWidth,  // 1000
       } = options;
 
-      function drawParagraph(ctx, text, x, y, stroke) {
-        const words = text.split(' ');
-        let str = words.shift();
-        do {
-          const word = words.shift() || '';
-          const newStr = `${str} ${word}`;
-          const m = ctx.measureText(newStr);
-          if (m.width > textWrapWidth || !word) {
-            let cut = '';
-            while (str.length) {
-              const m = ctx.measureText(str);
-              if (m.width <= textWrapWidth) {
-                break;
-              }
-              cut = `${str[str.length - 1]}${cut}`;
-              str = str.substr(0, str.length - 1);
-            }
-            if (stroke) {
-              ctx.strokeText(str, x, y);
-            } else {
-              ctx.fillText(str, x, y);
-            }
-            y += verticalSpacing;
-            if (cut) {
-              str = cut;
-              words.push(word);
-            } else {
-              str = word;
-            }
-          } else {
-            str = newStr;
-          }
-        } while (str);
+      function relativeOffset(offset) {
+        return [
+          offset[0] >= 0 ? offset[0] : backgroundImage.width  + offset[0],
+          offset[1] >= 0 ? offset[1] : backgroundImage.height + offset[1],
+        ];
       }
 
-      function drawShadowedParagraph(ctx, text, x, y) {
-        const color = ctx.fillStyle;
-        ctx.strokeStyle = 'black';
-        ctx.fillStyle = 'black';
-        drawParagraph(ctx, text, x + shadowOffset[0], y + shadowOffset[1], true);
-        drawParagraph(ctx, text, x + shadowOffset[0], y + shadowOffset[1]);
-        ctx.strokeStyle = 'white';
-        ctx.lineWidth = strokeWidth;
-        drawParagraph(ctx, text, x, y, true);
-        ctx.fillStyle = color;
-        drawParagraph(ctx, text, x, y);
-      }
-      
       const ctx = canvas.getContext('2d');
       ctx.drawImage(backgroundImage, 0, 0);
 
-      let s = 0;
-      for (let i = 0; i < text.length; ++i) {
-        s += text.codePointAt(i);
-      }
+      for (const t of text) {
+        const {
+          text,
+          font,
+          verticalSpacing, // 100
+          offset,  // 100, 120
+          shadowOffset,  // 15
+          strokeWidth,  // 15
+          textWrapWidth,  // 1000
+          color,
+          textAlign,
+        } = t;
 
-      ctx.font = font;
-      ctx.fillStyle = hsl(s / 100 % 1, 1, 0.4);
-      drawShadowedParagraph(ctx, text, offset[0], offset[1]);
+        function makeHueFromStr(text) {
+          let s = 0;
+          for (let i = 0; i < text.length; ++i) {
+            s += text.codePointAt(i);
+          }
+          return s / 100 % 1;
+        }
+
+
+        function drawParagraph(ctx, text, x, y, stroke) {
+          const words = text.split(' ');
+          let str = words.shift();
+          do {
+            const word = words.shift() || '';
+            const newStr = `${str} ${word}`;
+            const m = ctx.measureText(newStr);
+            if (m.width > textWrapWidth || !word) {
+              let cut = '';
+              while (str.length) {
+                const m = ctx.measureText(str);
+                if (m.width <= textWrapWidth) {
+                  break;
+                }
+                cut = `${str[str.length - 1]}${cut}`;
+                str = str.substr(0, str.length - 1);
+              }
+              if (stroke) {
+                ctx.strokeText(str, x, y);
+              } else {
+                ctx.fillText(str, x, y);
+              }
+              y += verticalSpacing;
+              if (cut) {
+                str = cut;
+                words.push(word);
+              } else {
+                str = word;
+              }
+            } else {
+              str = newStr;
+            }
+          } while (str);
+        }
+
+        function drawShadowedParagraph(ctx, text, x, y) {
+          const color = ctx.fillStyle;
+          ctx.strokeStyle = 'black';
+          ctx.fillStyle = 'black';
+          drawParagraph(ctx, text, x + shadowOffset[0], y + shadowOffset[1], true);
+          drawParagraph(ctx, text, x + shadowOffset[0], y + shadowOffset[1], false);
+          ctx.strokeStyle = 'white';
+          ctx.lineWidth = strokeWidth;
+          drawParagraph(ctx, text, x, y, true);
+          ctx.fillStyle = color;
+          drawParagraph(ctx, text, x, y);
+        }
+        
+        ctx.font = font;
+        ctx.fillStyle = color || hsl(makeHueFromStr(text), 1, 0.4);
+        ctx.textAlign = textAlign;
+        drawShadowedParagraph(ctx, text, ...relativeOffset(offset));
+      }
     };
 }));
